35e5f29f9919756615b97def80cd947464cf4f38
Arturoo0 <arturoportelles@gmail.com>
2019-12-30 18:23:30 -0500

    Changes to grid size

--- src/grid.lua
+++ src/grid.lua
@@ -7,8 +7,8 @@ function grid:draw()
   currentX = 0
   currentY = 0
 
-  for i = 1, 20 do
-    for i = 1, 20 do
+  for i = 1, 50 do
+    for i = 1, 50 do
       love.graphics.setColor(0,0,0)
       love.graphics.rectangle("line", currentX, currentY, self.size, self.size)
       currentX = currentX + self.size
