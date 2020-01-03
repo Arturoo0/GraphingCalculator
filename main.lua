79e2f771105bc373d619c77ed3f23717281417b7
Bruce Berrios <bberr022@fiu.edu>
2020-01-03 01:01:28 -0500

    Add textbox input

--- main.lua
+++ main.lua
@@ -1,89 +1,117 @@
+--
+-- local lg = love.graphics
+--
+-- WIDTH = lg:getWidth()
+-- HEIGHT = lg:getHeight()
+-- HALF_WIDTH = WIDTH * 0.5
+-- HALF_HEIGHT = HEIGHT * 0.5
+--
+-- color = require("src/color")
+-- local grid = require("src/grid")
+-- local camera = require("src/camera")
+--
+-- local graphCanvas = lg.newCanvas(grid.width, grid.height)
+--
+-- function love.load()
+--
+--   color:init {
+--     ["black-light"] = "#353b48",
+--     ["black-dark"] = "#2f3640",
+--     ["green-light"] = "#2ecc71",
+--     ["green-dark"] = "#27ae60",
+--     ["blue-light"] = "#3498db",
+--     ["blue-dark"] = "#2980b9",
+--     ["yellow-light"] = "#f1c40f",
+--     ["yellow-dark"] = "#f39c12",
+--     ["orange-light"] = "#e67e22",
+--     ["orange-dark"] = "#d35400",
+--     ["red-light"] = "#e74c3c",
+--     ["red-dark"] = "#c0392b",
+--     ["purple-light"] = "#9b59b6",
+--     ["purple-dark"] = "#8e44ad",
+--     ["white-light"] = "#ecf0f1",
+--     ["white-dark"] = "#bdc3c7",
+--     ["turquoise-light"] = "#1abc9c",
+--     ["turquoise-dark"] = "#16a085"
+--   }
+--
+--   camera:setPosition(grid.halfWidth - HALF_WIDTH, grid.halfHeight - HALF_HEIGHT)
+--
+--   camera:setBoundaries {
+--     minX = 0,
+--     minY = 0,
+--     maxX = grid.width - WIDTH,
+--     maxY = grid.height - HEIGHT
+--   }
+--
+--   color:setBackground("white-light")
+--
+--   local coords = {}
+--
+--   local function func(x)
+--     return x^3
+--   end
+--
+--   for x = -50, 50, 0.001 do
+--     coords[#coords + 1] = ((grid.tileSize / 2) * x) + grid.halfWidth
+--     coords[#coords + 1] = -((grid.tileSize / 2) * func(x)) + grid.halfHeight
+--   end
+--
+--   graphCanvas:renderTo(function()
+--     lg.clear()
+--     lg.setPointSize(6)
+--     color:set("purple-dark")
+--     lg.points(coords)
+--   end)
+-- end
+--
+-- function love.update(dt)
+--   camera:update(dt)
+-- end
+--
+-- local coords = {}
+--
+-- function love.draw()
+--   camera:set()
+--
+--   grid:draw()
+--
+--   color:set("white-light")
+--
+--   lg.setBlendMode("alpha", "premultiplied")
+--   lg.draw(graphCanvas)
+--   lg.setBlendMode("alpha")
+--
+--   camera:unset()
+--
+--   color:set("black-dark")
+--   lg.print(love.timer.getFPS())
+-- end
 
-local lg = love.graphics
 
-WIDTH = lg:getWidth()
-HEIGHT = lg:getHeight()
-HALF_WIDTH = WIDTH * 0.5
-HALF_HEIGHT = HEIGHT * 0.5
+local textbox = require("src/textbox")
 
-color = require("src/color")
-local grid = require("src/grid")
-local camera = require("src/camera")
+local myTB = textbox:new{
+  y = 150,
+  font = love.graphics.newFont(32)
+}
 
-local graphCanvas = lg.newCanvas(grid.width, grid.height)
-
-function love.load()
-
-  color:init {
-    ["black-light"] = "#353b48",
-    ["black-dark"] = "#2f3640",
-    ["green-light"] = "#2ecc71",
-    ["green-dark"] = "#27ae60",
-    ["blue-light"] = "#3498db",
-    ["blue-dark"] = "#2980b9",
-    ["yellow-light"] = "#f1c40f",
-    ["yellow-dark"] = "#f39c12",
-    ["orange-light"] = "#e67e22",
-    ["orange-dark"] = "#d35400",
-    ["red-light"] = "#e74c3c",
-    ["red-dark"] = "#c0392b",
-    ["purple-light"] = "#9b59b6",
-    ["purple-dark"] = "#8e44ad",
-    ["white-light"] = "#ecf0f1",
-    ["white-dark"] = "#bdc3c7",
-    ["turquoise-light"] = "#1abc9c",
-    ["turquoise-dark"] = "#16a085"
-  }
-
-  camera:setPosition(grid.halfWidth - HALF_WIDTH, grid.halfHeight - HALF_HEIGHT)
-
-  camera:setBoundaries {
-    minX = 0,
-    minY = 0,
-    maxX = grid.width - WIDTH,
-    maxY = grid.height - HEIGHT
-  }
-
-  color:setBackground("white-light")
-
-  local coords = {}
-
-  local function func(x)
-    return x^3
-  end
-
-  for x = -50, 50, 0.001 do
-    coords[#coords + 1] = ((grid.tileSize / 2) * x) + grid.halfWidth
-    coords[#coords + 1] = -((grid.tileSize / 2) * func(x)) + grid.halfHeight
-  end
-
-  graphCanvas:renderTo(function()
-    lg.clear()
-    lg.setPointSize(6)
-    color:set("purple-dark")
-    lg.points(coords)
-  end)
+function love.load(arg)
+  love.keyboard.setKeyRepeat(true)
 end
 
 function love.update(dt)
-  camera:update(dt)
+  myTB:update(dt)
 end
 
-local coords = {}
+function love.textinput(key)
+  myTB:getInput(key)
+end
+
+function love.keypressed(key)
+  myTB:keypressed(key)
+end
 
 function love.draw()
-  camera:set()
-
-  grid:draw()
-
-  color:set("white-light")
-
-  lg.setBlendMode("alpha", "premultiplied")
-  lg.draw(graphCanvas)
-  lg.setBlendMode("alpha")
-
-  camera:unset()
-
-  color:set("black-dark")
-  lg.print(love.timer.getFPS())
+  myTB:draw()
 end
--- src/textbox.lua
+++ src/textbox.lua
@@ -1,7 +1,10 @@
 -- Author: Bruce Berrios
 -- Description: A textbox class for user input
+local utf8 = require("utf8")
+
 local lg = love.graphics
-local concat = table.concat
+local concat, len, sub = table.concat, string.len, string.sub
+local cos = math.cos
 
 local textbox = {}
 
@@ -14,26 +17,73 @@ function textbox:new(properties)
     y = properties.y or 0,
     text = properties.text or "Hello World!",
     font = properties.font or lg:getFont(),
-    buffer = {},
-    backgroundColor = properties.backgroundColor or {1, 1, 1, 1}
-    textColor = properties.textColor or {0, 0, 0, 1}
+    backgroundColor = properties.backgroundColor or {1, 1, 1, 1},
+    textColor = properties.textColor or {0, 0, 0, 1},
+    focus = true,
   }
 
-  tb.width = properties.width or tb.font:getWidth("w") * 12
-  tb.height = properties.height or tb.font:getHeight() * 1.2
+  tb.fontHeight = tb.font:getHeight()
+
+  tb.width = properties.width or tb.font:getWidth(tb.text) * 2
+  tb.height = properties.height or tb.fontHeight * 1.2
+
+  tb.cursor = {
+    x = 0,
+    y = 0,
+    alpha = 1,
+    alphaTimer = 0,
+    position = len(tb.text)
+  }
 
   return setmetatable(tb, textbox)
 end
 
-function textbox:draw()
-
+function textbox:update(dt)
+  if(self.focus) then
+    self.cursor.alphaTimer = self.cursor.alphaTimer + dt
+    self.cursor.alpha = 1 * cos(7 * self.cursor.alphaTimer) + 1
+  else
+    self.cursor.alphaTimer = 0
+  end
 end
 
-function textbox:getInput(t)
+function textbox:draw()
+  lg.push("all")
+
+  lg.setColor(self.backgroundColor)
+  lg.rectangle("fill", self.x, self.y, self.width, self.height)
+
+  lg.setFont(self.font)
+  lg.setColor(self.textColor)
+  lg.print(self.text, self.x, self.y)
+
+  if(self.focus) then
+    lg.setColor(0, 0, 0, self.cursor.alpha)
+    lg.print("|", self.x + self.font:getWidth(self.text:sub(1, self.cursor.position)) - 2, self.y)
+  end
+
+  lg.pop()
+end
+
+function textbox:getInput(key)
+  if(self.focus) then
+    self.text = self.text .. key
+    self.cursor.position = self.cursor.position + 1
+  end
+end
+
+function textbox:keypressed(key)
+  if(key == "backspace") then
+    local byteoffset = utf8.offset(self.text, -1)
+    if (byteoffset) then
+      self.text = sub(self.text, 1, byteoffset - 1)
+      self.cursor.position = self.cursor.position - 1
+    end
+  end
 end
 
 function textbox:getText()
-  return concat(self.buffer)
+  return self.text
 end
 
 return textbox
