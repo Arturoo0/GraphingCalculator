ba7bc8ba2e48ce893d4826a6191c0f3dcf41ff5f
Bruce Berrios <bberr022@fiu.edu>
2020-01-09 01:55:11 -0500

    Simplify toggle logic

--- src/panel.lua
+++ src/panel.lua
@@ -10,17 +10,21 @@ local panel = {
   x = 0,
   y = 0,
   width = 300,
-  height = 300,
+  height = 700,
+  numInputs = 7,
   status = false,
-  buttonSize = 20,
-  buttonX = 5,
-  buttonY = 5,
+  button = {
+    x = 5,
+    y = 5,
+    width = 20,
+    height = 20,
+  },
   textboxes = {},
-  equations = {}
+  equations = {},
 }
 
 function panel:load()
-  for i = 1, 3 do
+  for i = 1, self.numInputs do
     self.textboxes[i] = textbox:new {
       width = self.width,
       height = 100,
@@ -28,74 +32,70 @@ function panel:load()
       text = "y = ",
       font = lg.newFont(24)
     }
-  end
-
-  for i = 1, 3 do
     self.equations[i] = equation:new()
   end
 end
 
 function panel:update(dt)
-  if(self.status) then
-    for i = 1, #self.textboxes do
-      self.textboxes[i]:update(dt)
-    end
+  if(not self.status) then return end
+
+  for i = 1, self.numInputs do
+    self.textboxes[i]:update(dt)
   end
 end
 
 function panel:draw()
+  local button = self.button
+
   if (self.status) then
     color:set("white-light")
     lg.rectangle("fill", 0, 0, self.width, self.height)
-    for i = 1, #self.textboxes do
+    for i = 1, self.numInputs do
       self.textboxes[i]:draw()
     end
   else
     color:set("black-light")
-    lg.rectangle("fill", self.buttonX, self.buttonY, self.buttonSize, self.buttonSize)
+    lg.rectangle("fill", button.x, button.y, button.width, button.height)
   end
 end
 
 function panel:keypressed(key)
   if(not self.status) then return end
-  for i = 1, #self.textboxes do
+
+  for i = 1, self.numInputs do
     self.textboxes[i]:keypressed(key)
   end
 end
 
 function panel:textinput(key)
   if(not self.status) then return end
-  for i = 1, #self.textboxes do
+
+  for i = 1, self.numInputs do
     self.textboxes[i]:getInput(key)
   end
 end
 
 function panel:mousepressed(x, y, button)
   if(not self.status) then return end
-  for i = 1, #self.textboxes do
+
+  for i = 1, self.numInputs do
     self.textboxes[i]:mousepressed(x, y, button)
   end
 end
 
+local function intersects(x, y, box)
+  local intersectsX = (x >= box.x and x <= (box.x + box.width))
+  local intersectsY = (y >= box.y and y <= (box.y + box.height))
+
+  return intersectsX and intersectsY
+end
+
 function panel:mousereleased(x, y, button)
-  -- saves reused expressions
-  local intersectsX = (x >= self.buttonX and x <= (self.buttonX + self.buttonSize))
-  local intersectsY = (y >= self.buttonY and y <= (self.buttonY + self.buttonSize))
+  if(button ~= 1) then return end
 
-  if (intersectsX and intersectsY) then
-    if (button == 1 and self.status == false) then
-      self.status = true
-    end
-  end
+  local target = (self.status) and self or self.button
 
-  intersectsX = ((x >= self.x and x <= (self.x + self.width)))
-  intersectsY = ((y >= self.y and y <= (self.y + self.height)))
-
-  if (self.status) then
-    if (not (intersectsX and intersectsY)) then
-      self.status = false
-    end
-  end
+  self.status = intersects(x, y, target)
 end
 
 return panel
