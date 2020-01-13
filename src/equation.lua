fad35f06723f7359edd94caf7c80d1b53997dc9b
Bruce Berrios <bberr022@fiu.edu>
2020-01-13 01:43:33 -0500

    Add userinput

--- src/equation.lua
+++ src/equation.lua
@@ -1,11 +1,14 @@
+-- Author: Bruce Berrios
+-- Description: An equation class
 
 local lg = love.graphics
-local insert = table.insert
 
 local grid = require("src/grid")
 
-local equation = {}
+local NUM_COORDS = 101000
+local set = rawset
 
+local equation = {}
 equation.__index = equation
 
 function equation:new(properties)
@@ -18,12 +21,23 @@ function equation:new(properties)
     valid = false,
   }
 
-  for x = -50, 50, 0.001 do
-    insert(eq.coords, ((grid.tileSize / 2) * x) + grid.halfWidth)
-    insert(eq.coords, -((grid.tileSize / 2) * eq.func(x)) + grid.halfHeight)
-  end
-
   return setmetatable(eq, equation)
+end
+
+function equation:recomputeCoords(func)
+  for i = 1, NUM_COORDS do self.coords[i] = nil end
+
+  self.valid = true
+  self.func = func
+
+  local index = 1
+
+  for x = -50, 50, 0.001 do
+    set(self.coords, index, ((grid.tileSize / 2) * x) + grid.halfWidth)
+    set(self.coords, index + 1, -((grid.tileSize / 2) * self.func(x)) + grid.halfHeight)
+
+    index = index + 2
+  end
 end
 
 function equation:getCoords()
