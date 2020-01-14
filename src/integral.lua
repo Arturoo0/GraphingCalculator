a340d834fe3dede9ddde6b3bf9f9a2e41d36376c
Bruce Berrios <bberr022@fiu.edu>
2020-01-14 13:34:23 -0500

    Change function access and names

--- src/integral.lua
+++ src/integral.lua
@@ -4,27 +4,23 @@
 local floor = math.floor
 local lg = love.graphics
 
-local function getTrueX(x)
-  local trueX = ((x - grid.halfWidth) * 2) / grid.tileSize
-  return trueX
-end
-
-local function getTrueY(y)
-  local trueY = ((y - grid.halfHeight) * -2) / grid.tileSize
-  return trueY
-end
-
-local function computeIntegral(coords)
+local function computeRiemannSum(func)
   local sum = 0
+  local y = 0
 
-  for i = 1, 200000, 2 do
-    sum = sum + (getTrueX(coords[i + 2]) - getTrueX(coords[i])) * getTrueY(coords[i + 1])
+  for x = -50, 50, 0.001 do
+    y = func(x)
+    sum = sum + ((x + 0.001) - x) * y
   end
 
   return sum
 end
 
-function Integral(equation)
+local function truncate(x)
+  return floor(x * 1000) / 1000
+end
+
+local function drawIntegral(equation)
 
   local coords = equation:getCoords()
 
@@ -33,6 +29,8 @@ function Integral(equation)
   end
 
   lg.setColor(0,0,0)
-  lg.print("Definite Integral " .. floor(computeIntegral(coords) * 1000)/1000, 805, 540, r, 1.40, 1.40)
+  lg.print("Definite Integral " .. truncate(computeRiemannSum(equation.func)), 805, 540, r, 1.40, 1.40)
 
 end
+
+return drawIntegral
